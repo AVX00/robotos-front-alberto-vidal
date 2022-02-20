a840ff0af1eea716fe50947ec0230f7537dfe07f
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-20 14:08:29 +0100

    created robots reducer, moved actions to robots folder

---
+++ src/redux/reucers/robots/robotsReducer.js
@@ -0,0 +1,30 @@
+import robotsActionTypes from "../../actions/robots/robotsActionTypes";
+
+const robotsReducer = (robots, action) => {
+  let newRobots;
+
+  switch (action.type) {
+    case robotsActionTypes.createRobot:
+      newRobots = [...robots, action.robot];
+      break;
+    case robotsActionTypes.deleteRobot:
+      newRobots = robots.filter((robot) => robot.id !== action.id);
+      break;
+    case robotsActionTypes.loadRobots:
+      newRobots = [...robots];
+      break;
+    case robotsActionTypes.updateRobot:
+      newRobots = robots.map((robot) =>
+        robot.id === action.robot.id ? action.robot : robot
+      );
+      break;
+
+    default:
+      newRobots = [...robots];
+      break;
+  }
+
+  return newRobots;
+};
+
+export default robotsReducer;
