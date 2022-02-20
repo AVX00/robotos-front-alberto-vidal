576535483380b17918c9cec0f3e88ade5501282d
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-20 17:51:37 +0100

    cerated new robot date input

--- src/components/NewRobot/NewRobot.js
+++ src/components/NewRobot/NewRobot.js
@@ -25,6 +25,26 @@ const NewRobot = () => {
           <opiton value="0">0</opiton>
         </select>
       </div>
+      <div className="form-group">
+        <label htmlFor="resistance">Resistance</label>
+        <select name="resistance" id="resistance">
+          <opiton value="">--select one--</opiton>
+          <opiton value="1">1</opiton>
+          <opiton value="2">2</opiton>
+          <opiton value="3">3</opiton>
+          <opiton value="4">4</opiton>
+          <opiton value="5">5</opiton>
+          <opiton value="6">6</opiton>
+          <opiton value="7">7</opiton>
+          <opiton value="8">8</opiton>
+          <opiton value="9">9</opiton>
+          <opiton value="0">0</opiton>
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="fabrication-date">Image URL</label>
+        <input id="fabrication-date" type="date" value="" />
+      </div>
     </form>
   );
 };
