c118edfa70dd96286df0369e9d1fde6feb7da45b
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-20 18:27:42 +0100

    created form data managment system

--- src/components/NewRobot/NewRobot.js
+++ src/components/NewRobot/NewRobot.js
@@ -1,17 +1,73 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { createRobotThunkCreator } from "../../redux/thunks/robotsThunks";
+
+const RobotForm = styled.form`
+  color: #f0f0f0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+  & > input {
+    width: 100%;
+  }
+`;
+
 const NewRobot = () => {
+  const blankData = {
+    name: "",
+    img: "",
+    speed: "",
+    resistance: "",
+    "fabrication-date": "",
+  };
+  const [formData, setFormData] = useState(blankData);
+  const handleDataChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
+
+  const dispatch = useDispatch();
+
+  const handleSubmitEvent = (event) => {
+    event.preventDefault();
+    const {
+      name,
+      img,
+      speed,
+      resistance,
+      "fabrication-date": fabricationDate,
+    } = formData;
+    const robot = { name, img, stats: { speed, resistance, fabricationDate } };
+    dispatch(createRobotThunkCreator(robot));
+  };
+
   return (
-    <form>
+    <RobotForm onSubmit={handleSubmitEvent}>
       <div className="form-group">
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
+        <input
+          id="name"
+          type="text"
+          autoComplete="new-password"
+          placeholder="Robot Name"
+          onChange={handleDataChange}
+          value={formData.name}
+        />
       </div>
       <div className="form-group">
         <label htmlFor="img">Image URL</label>
-        <input id="img" type="text" />
+        <input
+          id="img"
+          type="text"
+          onChange={handleDataChange}
+          autoComplete="new-password"
+          placeholder="Robot Image"
+        />
       </div>
       <div className="form-group">
         <label htmlFor="speed">Speed</label>
-        <select name="speed-select" id="speed">
+        <select name="speed-select" id="speed" onChange={handleDataChange}>
           <option value="">--select one--</option>
           <option value="1">1</option>
           <option value="2">2</option>
@@ -27,7 +83,11 @@ const NewRobot = () => {
       </div>
       <div className="form-group">
         <label htmlFor="resistance">Resistance</label>
-        <select name="resistance-select" id="resistance">
+        <select
+          name="resistance-select"
+          id="resistance"
+          onChange={handleDataChange}
+        >
           <option value="">--select one--</option>
           <option value="1">1</option>
           <option value="2">2</option>
@@ -43,9 +103,10 @@ const NewRobot = () => {
       </div>
       <div className="form-group">
         <label htmlFor="fabrication-date">Image URL</label>
-        <input id="fabrication-date" type="date" value="" />
+        <input id="fabrication-date" type="date" onChange={handleDataChange} />
       </div>
-    </form>
+      <button type="submit">Create</button>
+    </RobotForm>
   );
 };
 
